5192f3088f123300c57d6d1716117c34f17e2499
AKASH-18 <akashakashbisht@gmail.com>
2026-02-23 13:36:22 +0530

    update

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+
 dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(express.json());
+
+// routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/books", require("./routes/bookRoutes"));
 
+// test route
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+// 404 handler
 app.use((req, res) => {
   res.status(404).json({
     success: false,
@@ -16,5 +26,4 @@ app.use((req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
