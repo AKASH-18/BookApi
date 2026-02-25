bc24f44fc8bdcca5cc66391519f153a38ba43dff
Akash Bisht <56999019+AKASH-18@users.noreply.github.com>
2026-02-25 06:12:46 +0530

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -3,45 +3,69 @@ const Counter = require("./Counter");
 
 const bookSchema = new mongoose.Schema(
   {
+    // Auto Increment Book ID
     bookId: {
       type: Number,
       unique: true,
     },
+
     title: {
       type: String,
-      required: true,
+      required: [true, "Title is required"],
+      trim: true,
     },
+
     author: {
       type: String,
-      required: true,
+      required: [true, "Author is required"],
+      trim: true,
     },
+
     genre: {
       type: String,
+      trim: true,
     },
+
     price: {
       type: Number,
-      required: true,
+      required: [true, "Price is required"],
     },
+
     inStock: {
       type: Boolean,
       default: true,
     },
+
+    // IMPORTANT: make OPTIONAL (not required)
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      default: null,
     },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
-bookSchema.pre("save", async function () {
-  if (!this.isNew) return;
-  const counter = await Counter.findOneAndUpdate(
-    { name: "bookId" },
-    { $inc: { seq: 1 } },
-    { new: true, upsert: true },
-  );
-  this.bookId = counter.seq;
+
+
+/* ===============================
+   AUTO-INCREMENT bookId
+================================ */
+bookSchema.pre("save", async function (next) {
+  if (!this.isNew) return next();
+
+  try {
+    const counter = await Counter.findOneAndUpdate(
+      { name: "bookId" },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+
+    this.bookId = counter.seq;
+    next();
+
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
