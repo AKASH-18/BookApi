f719be1dc7563827c68a4a48dd7fcafac834d798
AKASH-18 <akashakashbisht@gmail.com>
2026-02-25 10:07:37 +0530

    added validation middleware

--- server.js
+++ server.js
@@ -33,3 +33,15 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+app.use(limiter);
